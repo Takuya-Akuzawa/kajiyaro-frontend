cbca07345f8d20fd4fbe171054405d0be0a529ab
Takuya Akuzawa <kuat2245@gmail.com>
2022-05-02 13:52:40 +0900

    追加コミット

--- pages/housework-page.js
+++ pages/housework-page.js
@@ -1,9 +1,24 @@
 import Layout from "../components/Layout";
+import Housework from "../components/Housework";
+import { getAllHouseworkData } from "../lib/houseworks";
 
-export default function HouseworkList() {
+export default function HouseworkList({ houseworks }) {
   return (
     <Layout title="Housework">
       <div>Housework List</div>
+      <ul>
+        {houseworks &&
+          houseworks.map((housework) =>
+            <Housework key={housework.id} housework={housework} />)}
+      </ul>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const houseworks = await getAllHouseworkData()
+
+  return {
+    props: { houseworks }
+  }
 }
