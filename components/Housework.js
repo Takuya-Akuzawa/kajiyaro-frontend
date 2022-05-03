3691f0e8690f990e864840c860075e19d2d82d8a
Takuya Akuzawa <kuat2245@gmail.com>
2022-05-03 12:13:06 +0900

    KA-14 カード形式のリストとしてHouseworkを表示

--- components/Housework.js
+++ components/Housework.js
@@ -1,11 +1,18 @@
 export default function Housework({ housework }) {
   return (
-    <div>
-      <span>{housework.id}</span>
-      {" : "}
-      <span className="cursor-pointer text-gray-700 border-b border-gray-500">
-        {housework.housework_name}
-      </span>
+    <div
+      className="cursor-pointer border rounded-md mb-3
+                border-indigo-400  flex justify-items-center items-center contents-center ">
+      <div className="px-7 py-5 border-r  border-indigo-300 w-auto">{housework.category}</div>
+      <div className="w-60 py-2 text-center">
+        <span className="text-lg font-semibold text-gray-700 align-middle border-b border-gray-500">
+          {housework.housework_name}
+        </span>
+      </div>
+      <div className="pr-4">
+        <div className="text-sm">{"標準"}</div>
+        <div className="text-sm">{housework.estimated_time}分</div>
+      </div>
     </div>
   )
 }
