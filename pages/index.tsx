140684d3bd5a5ac654e4a24d5ba247dacb1a3f7f
Takuya Akuzawa <kuat2245@gmail.com>
2022-05-15 18:24:18 +0900

    KA-8 typescript導入

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Dashboard from '../components/Dashboard'
+import Layout from '../components/Layout'
+
+const Home: React.FC = () => {
+  return (
+    <Layout title="Dashboard">
+      <Dashboard />
+    </Layout>
+  )
+}
+
+export default Home
