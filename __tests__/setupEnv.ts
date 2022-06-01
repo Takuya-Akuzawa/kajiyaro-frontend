8948ae7e0018965d778ae099e7de3c9d480dd3d3
Takuya Akuzawa <kuat2245@gmail.com>
2022-06-02 00:03:12 +0900

    KA-4  setupEnv.ts更新 hide error messages about act() being unsupported in production build

--- __tests__/setupEnv.ts
+++ __tests__/setupEnv.ts
@@ -4,3 +4,14 @@ export default async (): Promise<void> => {
   const projectDir = process.cwd()
   loadEnvConfig(projectDir)
 }
+
+// hide error messages about act() being unsupported in production build
+const ignoredErrors = [
+  /act(...) is not supported in production builds of React, and might not behave as expected./,
+]
+const consoleError = global.console.error
+global.console.error = (...args) => {
+  if (ignoredErrors.some((el) => el.test(args[0]))) {
+    return consoleError(...args)
+  }
+}
