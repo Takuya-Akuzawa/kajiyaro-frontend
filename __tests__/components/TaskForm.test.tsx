5789f3204b632a254bdfcab5eb3af12dce90b313
Takuya Akuzawa <kuat2245@gmail.com>
2022-06-26 23:41:35 +0900

    KA-32 TaskFormコンポーネントの更新テスト実装

--- __tests__/components/TaskForm.test.tsx
+++ __tests__/components/TaskForm.test.tsx
@@ -17,6 +17,12 @@ const handlers = [
       return res(ctx.status(201))
     }
   ),
+  rest.put(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/:id`,
+    (req, res, ctx) => {
+      return res(ctx.status(200))
+    }
+  ),
   rest.get(
     `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-category/`,
     (req, res, ctx) => {
@@ -60,8 +66,23 @@ describe('Task登録フォームのコンポーネント単体テスト', () => 
       username: 'Takuya',
     },
     scheduled_date: '2022-06-12',
-    result_date: undefined,
-    result_time: undefined,
+    result_date: '',
+    result_time: 0,
+  }
+  const updateTask = {
+    task_name: 'お風呂掃除',
+    category: {
+      id: 3,
+      category_name: '住',
+    },
+    status: '完了',
+    assigned_user: {
+      id: 3,
+      username: 'Narumi',
+    },
+    scheduled_date: '2022-06-12',
+    result_date: '2022-06-12',
+    result_time: 10,
   }
 
   it('フォーム入力欄にTask内容を入力して、登録ボタン実行が成功する事', async () => {
@@ -147,6 +168,87 @@ describe('Task登録フォームのコンポーネント単体テスト', () => 
 
     expect(
       await screen.findByText('タスクの登録に失敗しました')
+    ).toBeInTheDocument()
+  })
+  it('フォームの入力内容を変更し、更新ボタン実行が成功する事', async () => {
+    document.cookie = 'access_token=123xyz'
+    render(
+      <SWRConfig value={{ dedupingInterval: 0 }}>
+        <TaskForm />
+      </SWRConfig>,
+      { wrapper: TaskContextProvider }
+    )
+    // フォーム入力前は更新Buttonが非活性になっている事
+    const createButton = screen.getByText('更新')
+    expect(createButton).toBeDisabled()
+
+    // 各入力フォームに入力できる事
+    const taskInput = screen.getByPlaceholderText('タスク名')
+    userEvent.clear(taskInput)
+    userEvent.type(taskInput, updateTask.task_name)
+    expect(screen.getByDisplayValue(updateTask.task_name)).toBeInTheDocument()
+
+    // Categoryドロップダウンのfetchができるまで待つ
+    await screen.findByText('衣')
+    userEvent.selectOptions(screen.getByDisplayValue('衣'), ['2'])
+    expect(
+      screen.getByDisplayValue(updateTask.category['category_name'])
+    ).toBeInTheDocument()
+
+    // 担当者を変更
+    const inputUser = await screen.findByText(/担当者/)
+    userEvent.selectOptions(
+      inputUser,
+      updateTask.assigned_user['id'].toString()
+    )
+
+    // ステータスの入力値をクリアして”完了”にする
+    const inputStatus = screen.getByPlaceholderText('ステータス')
+    userEvent.clear(inputStatus)
+    userEvent.type(inputStatus, updateTask.status)
+    expect(screen.getByDisplayValue(updateTask.status)).toBeInTheDocument()
+
+    // 実績日を入力
+    fireEvent.change(screen.getByPlaceholderText('実績日'), {
+      target: { value: updateTask.scheduled_date },
+    })
+    expect(screen.getByDisplayValue(updateTask.result_date)).toBeInTheDocument()
+
+    // 実績時間を入力
+    const inputResultTime = screen.getByPlaceholderText('実績時間')
+    userEvent.clear(inputResultTime)
+    userEvent.type(inputResultTime, updateTask.result_time.toString())
+
+    // 更新Buttonが活性化され、更新実行後に更新成功メッセージが表示される事
+    const updateButton = screen.getByText('更新')
+    expect(updateButton).toBeEnabled()
+    userEvent.click(updateButton)
+    expect(await screen.findByText('タスクを更新しました')).toBeInTheDocument()
+  })
+
+  it('バックエンドのサーバエラー時、更新処理に失敗しその旨メッセージが表示される事', async () => {
+    document.cookie = 'access_token=123xyx'
+    server.use(
+      rest.put(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/:id`,
+        (req, res, ctx) => {
+          return res(ctx.status(503))
+        }
+      )
+    )
+
+    // ステータスの入力値をクリアして”完了”にする
+    const inputStatus = screen.getByPlaceholderText('ステータス')
+    userEvent.clear(inputStatus)
+    userEvent.type(inputStatus, updateTask.status)
+    expect(screen.getByDisplayValue(updateTask.status)).toBeInTheDocument()
+
+    // 更新Buttonが活性化され、更新実行後に更新失敗メッセージが表示される事
+    const updateButton = screen.getByText('更新')
+    expect(updateButton).toBeEnabled()
+    userEvent.click(updateButton)
+    expect(
+      await screen.findByText('タスクの更新に失敗しました')
     ).toBeInTheDocument()
   })
 })
